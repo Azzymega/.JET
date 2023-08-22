63bac8c2c588e5502b6e3565e541f7e972ef077c
RedDawn <jdjdjd33loxru@gmail.com>
2023-08-22 19:55:42 +0300

    Initial commit

---
+++ Core/Def.h
@@ -0,0 +1,11 @@
+//
+// Created by Danil on 22.08.2023.
+//
+
+#ifndef JET_DEF_H
+#define JET_DEF_H
+
+#include <cstdint>
+
+typedef uint64_t AInteger;
+#endif //JET_DEF_H
---
+++ Core/Thread.h
@@ -0,0 +1,13 @@
+//
+// Created by Danil on 22.08.2023.
+//
+
+#ifndef JET_THREAD_H
+#define JET_THREAD_H
+
+#include "Def.h"
+
+struct Thread {
+    AInteger ProgramCounter;
+};
+#endif //JET_THREAD_H
